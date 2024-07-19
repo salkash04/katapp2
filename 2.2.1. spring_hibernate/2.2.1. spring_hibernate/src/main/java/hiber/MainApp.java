9aea0b43ae004b819b2942883b3b09978aca200d
Салават <salkash04@mail.ru>
2024-07-19 17:20:41 +0300

    initial

--- 2.2.1. spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -16,7 +16,7 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Niisan", 320)));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Nisan", 320)));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Toyota",54)));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Volvo",23)));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("BMW",9)));
